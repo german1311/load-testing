5013cb03d0d51e1b8ea0cf5357338330340ef8bd
desarrollador263_usi <desarrollador263_usi@minedu.gob.pe>
2024-03-14 14:41:24 -0500

    log config

--- src/test/scala/com/minedu/loadtest/MySimulation.scala
+++ src/test/scala/com/minedu/loadtest/MySimulation.scala
@@ -10,10 +10,7 @@ import scala.math.Integral.Implicits.infixIntegralOps
 import scala.math.Numeric.Implicits.infixNumericOps
 
 class MySimulation extends Simulation {
-  def randomNumber() = Random.nextInt(Integer.MAX_VALUE)
-
   val namesData = Iterator.continually(
-    // Random number will be accessible in session under variable "OrderRef"
     Map("nombres" -> s"Automation${Random.nextInt(Integer.MAX_VALUE)}")
   )
 
@@ -26,25 +23,7 @@ class MySimulation extends Simulation {
     .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
 
   val token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTA0NDM5NTAsImp0aSI6ImFlYzI1MWZkLTNhNTAtNDI3MS1iOTRkLTJkOGQ1M2RhMDI1ZSIsInJkayI6Ijg5YTNhMDQ3ODhiMzRiZDY4YjU5Yzg5MDE2NmNiOGEzIiwidXNyIjoiMTM5MTMxNiIsInNlZCI6IjI4NjMwIiwicm9sIjoiOTUzOSIsImNybCI6IlIwMV8yNiIsImNtbCI6IjA1NDYzOTAiLCJhbngiOiIwIiwiYWd3IjoibnNpYWdpZS0yMDQ4IiwiaWRTZXNzaW9uIjoiODlhM2EwNDc4OGIzNGJkNjhiNTljODkwMTY2Y2I4YTMiLCJuYmYiOjE3MTA0NDM5NTAsImV4cCI6MTcxMDQ0NDk3MCwiaXNzIjoibnNpYWdpZS0yMDQ4IiwiYXVkIjoibWluZWR1LnNpYWdpZS52MiJ9.Tv-r6V8X_l2z8WhPzL9PN5MR6SRk_Gln5dLamhzS_uhqtGK7iX-TXiKdQ2EsVlWhaoBcDwlRrDQB0qkYxuujrKKWvxtYbjincT7YIcD7uxwiO8d9BuONiKmkE-MXfKZYXQ4C6vOnigGScBLF51wwNgBKrYIGpa-rReGhOgfQQDH85jE_667AKEzvl_mPbtBF4CYMkITXk_568S86DGLACXuIhuXGyiZI-Ovq9Pbc1mAXEnuGJdXzMycmkzFQz1MYdODqM_nGiScHOW7etYycLUdtuWrfUnHAvm2SqrzZNfHV8CpB81Zl9aDRmDjMbX5mhH_moaCOc0Wzslp4vwpUMA"
-  val header_login = Map(
-    "Connection" -> "keep-alive",
-    "x-requested-with" -> "Fetch",
-    "authority" ->"10.200.13.59:5433",
-    "accept"->"application/json, text/plain, */*",
-    "accept-language"->"en-US,en;q=0.9",
-    "codigo-rol"->"R01_16",
-    "codigo-sede"->"0546390",
-    "content-type"->"application/json",
-    "origin"->"https://web.dev.siagie.in.otic.pe",
-    "referer"->"https://web.dev.siagie.in.otic.pe/",   
-    "sec-ch-ua"->""""Chromium";v="122", "Not(A:Brand";v="24", "Microsoft Edge";v="122"""",
-    "sec-ch-ua-mobile"->"?0",
-    "sec-ch-ua-platform"->"Windows",
-    "sec-fetch-dest"->"empty",
-    "sec-fetch-mode"->"cors",
-    "sec-fetch-site"->"same-site",
-    "user-agent"->"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0"
-    )
+
   val header_accept_json = Map(
     "Connection" -> "keep-alive",
     "Authorization" -> s"Bearer ${token}",
@@ -64,20 +43,9 @@ class MySimulation extends Simulation {
     "sec-fetch-site"->"same-site",
     "user-agent"->"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0"
     )
-  
+
   val scn = scenario("BasicSimulation")
     .feed(namesData)
-    // .exec(http("login")
-    //   .post("/auth")
-    //   .headers(header_login)
-    //   .body(StringBody("""{
-    //       "user": "70140602",
-    //       "clave": "gvaHJfHwLgO2hlWHvPmKrdfRCjDLV6LKaUo861ygdUJGqo0Q40nwo1AMY4CgdgxnaOU2ytBD7jmIQNZle6dS1vUZvJ6f5Ku4Z5ixTqBEsC1z2vs5QCZQIfduf/kOlDpFjd0MnJCIoBDlgHYA65mz0fkBv0Hqj7J32AeMBZIpzL57PSdV9iGOmU3Ef3mET8VrSHu/kNHtZgqYTb5wkKpShw+ahxGp7zXwAOuGHeBc1kOoc33CzyHxjauNmQip3lFpPaw1T6kk9yw5q+dDilbtjPmKRp3yp/O03YJtQl4h8JkGxRd0rh935lVL9ZrdK7QMPS/Ff7f3Xtp7vdxj8jmyig==",
-    //       "captcha": "",
-    //       "captchaKey": ""
-    //   }"""))
-    //   .check(jsonPath("$.accessToken").find.saveAs("token"))
-    // )
     .exec(http("post_ambientes")
       .post("/api/horas-grados/ambientes")
       .headers(header_accept_json)
@@ -98,11 +66,6 @@ class MySimulation extends Simulation {
       }"""))
       .check(bodyString.saveAs("BODY"))
     )
-    // .exec(session => {
-    //   val response = session("BODY").as[String]
-    //   println(s"Response body: \n$response")
-    //   session
-    // })
     .pause(5)
     .exec(http("patch_ambiente")
       .put("/api/horas-grados/ambientes/2808/1")
@@ -125,11 +88,6 @@ class MySimulation extends Simulation {
       }"""))
       .check(bodyString.saveAs("BODY"))
     )
-    // .exec(session => {
-    //   val response = session("BODY").as[String]
-    //   println(s"Response body: \n$response")
-    //   session
-    // })
     .pause(5)
     .exec(http("post_secciones")
       .post("/api/horas-grados/secciones")
@@ -171,11 +129,6 @@ class MySimulation extends Simulation {
       }"""))
       .check(bodyString.saveAs("BODY"))
     )
-    // .exec(session => {
-    //   val response = session("BODY").as[String]
-    //   println(s"Response body: \n$response")
-    //   session
-    // })
     .pause(5)
     .exec(http("patch_seccion")
       .put("/api/horas-grados/secciones/2808/1")
@@ -216,18 +169,12 @@ class MySimulation extends Simulation {
           "idPrograma": 180,
           "observaciones": ""
       }"""))
-      .check(bodyString.saveAs("BODY"))
     )
-    // .exec(session => {
-    //   val response = session("BODY").as[String]
-    //   println(s"Response body: \n$response")
-    //   session
-    // })
 
   setUp(
     scn.inject(
-      constantConcurrentUsers(10).during(10), // 1
-      rampConcurrentUsers(10).to(20).during(10) // 2
+      constantConcurrentUsers(2).during(10)
+      // rampConcurrentUsers(10).to(20).during(10)
       )
       .protocols(httpProtocol)
     )    
